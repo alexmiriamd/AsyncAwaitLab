85aa9c5572254b8a92682d70f2f4d5c6c1e6f1dd
andre <andre.jacques.pujol@gmail.com>
2023-02-10 10:49:03 -0600

    Error handeling finished

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
-const { readFile } = require('fs').promises;
+const { readFile } = require("fs").promises;
 
 const mostFrequentWord = (text) => {
   const words = text.toLowerCase().match(/[^_\W]+/g);
   const tally = {};
   let mostFrequentWord = null;
-  
-  words.forEach(word => {
-    tally[word] = (tally[word] || 0) + 1 ;
-    if(!tally[mostFrequentWord] || tally[word] > tally[mostFrequentWord])
+
+  words.forEach((word) => {
+    tally[word] = (tally[word] || 0) + 1;
+    if (!tally[mostFrequentWord] || tally[word] > tally[mostFrequentWord])
       mostFrequentWord = word;
   });
   return mostFrequentWord;
-}
+};
 
 const findPassword = async () => {
   // Your code goes here
-   const poem1 = await readFile("poems/starting-poem.txt", "utf-8");
-   const poem2 = mostFrequentWord(poem1);
-   const poem3 = mostFrequentWord(poem2);
-   let password = mostFrequentWord(poem3);
-   console.log(password);
-}
+  let poem1;
+  try {
+    poem1 = await readFile("poems/starting-poem.txt", "utf-8");
+  } catch (err) {
+    console.log("Could not read file: ", err.stack);
+    return;
+  }
+  const poem2 = mostFrequentWord(poem1);
+  const poem3 = mostFrequentWord(poem2);
+  let password = mostFrequentWord(poem3);
+  console.log(password);
+};
 
 findPassword();
